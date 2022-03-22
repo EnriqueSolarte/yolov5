2d1846ab7f6b7214bc8920e6faa0cc59b9dc74c7
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2022-03-22 10:09:50 +0000

    WIP: Backup from BEAST SERVER

--- train_rod_detector.sh
+++ train_rod_detector.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python train.py --img 640 --batch 16 --epochs 500 --data rod_dataset.yaml  --weights yolov5s.pt
+python train.py --img 640 --batch 16 --epochs 500 --data rod_dataset.yaml  --weights yolov5s.pt --project rod_project --name defualt --device 0
